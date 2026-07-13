58b877e807d53af6a31c191ceca62f1ac342a818
saichaithanya07005 <sai.chaithanya@prodapt.com>
2026-07-13 21:59:44 +1300

    Day 6

--- day6/count_vowels.py
+++ day6/count_vowels.py
@@ -2,4 +2,5 @@ def count_vowels(text):
     count = text.count('a') + text.count('e') + text.count('i') + text.count('o') + text.count('u') + text.count('A') + text.count('E') + text.count('I') + text.count('O') + text.count('U')
     return count
 
-print(count_vowels("Hello, World!"))
+text=input("Enter a string: ")
+print(count_vowels(text))
