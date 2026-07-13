0a9d6dd93393268d9cf9a31833ee25c15ce4c0f9
saichaithanya07005 <sai.chaithanya@prodapt.com>
2026-07-13 22:16:22 +1300

    Day 6

--- day6/longest.py
+++ day6/longest.py
@@ -5,3 +5,6 @@ for word in words:
     if len(word) > len(longest_word):
         longest_word = word
 print("The longest word is:", longest_word)
+
+"""we can use this also to find the longest word in a sentence. The code takes a sentence as input, splits it into words, and then iterates through each word to find the longest one. Finally, it prints the longest word found in the sentence.
+longest_word = max(words, key=len)"""
