ccd24a2ccbecd1445460c22bf5de4bdaca81d6bc
saichaithanya07005 <sai.chaithanya@prodapt.com>
2026-07-15 18:36:39 +1300

    Day 8

--- day8/addition.py
+++ day8/addition.py
@@ -1,3 +1,40 @@
 def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return a / b
+
+def  main():
+    print("This is a simple calculator module. You can use the add, subtract, multiply, and divide functions.")
+    print("Select a choice:")
+    print("1. Addition")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    choice = int(input("Enter your choice (1-4): "))
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    if choice == 1:
+        print(f"The result of addition is: {add(num1, num2)}")
+    elif choice == 2:
+        print(f"The result of subtraction is: {subtract(num1, num2)}")
+    elif choice == 3:
+        print(f"The result of multiplication is: {multiply(num1, num2)}")
+    elif choice == 4:
+        try:
+            result = divide(num1, num2)
+            print(f"The result of division is: {result}")
+        except ValueError as e:
+            print(e)
+
+if __name__ == "__main__":
+    main()
--- day8/test_addition.py
+++ day8/test_addition.py
@@ -1,4 +1,4 @@
-from addition import add
+from addition import add, subtract, multiply, divide
 
 def test_add_positive_numbers():
     assert add(2, 3) == 5
@@ -14,3 +14,30 @@ def test_add_positive_and_negative():
 
 def test_add_floats():
     assert add(2.5, 3.1) == 5.6
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_subtract_negative_numbers():
+    assert subtract(-5, -3) == -2
+
+def test_subtract_zero():
+    assert subtract(5, 0) == 5
+
+
+def test_multiply():
+    assert multiply(5, 4) == 20
+
+def test_multiple_zero():
+    assert multiply(0, 4) == 0
+
+def test_divide():
+    assert divide(6, 3) == 2
+
+def test_divide_by_zero():
+    try:
+        divide(5, 0)
+    except ZeroDivisionError:
+        pass
+    else:
+        assert False, "Expected ZeroDivisionError"
