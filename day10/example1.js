d574284ebf2f88a5a31752f81b781ca389288cbb
saichaithanya07005 <sai.chaithanya@prodapt.com>
2026-07-17 17:23:26 +1300

    Day 10

---
+++ day10/example1.js
@@ -0,0 +1,9 @@
+let name = "abc";
+const age = 30;
+var location = "chennai";
+
+let name = "xy";
+var location = "mumbai";
+
+console.log(name);
+console.log(location);
---
+++ day10/example2.js
@@ -0,0 +1,22 @@
+let name = "Alice"
+let age = 25
+let isActive = true
+let salary = null
+let address
+let id = Symbol("id")
+let big = 123456789n
+
+//Non Primitive
+let student = {
+    name: "John",
+    age: 22
+};
+
+let colors = ["Red", "Blue"]
+
+function greet()
+{
+    console.log("Hello")
+}
+
+greet()
